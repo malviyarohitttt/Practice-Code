f98241dd4024da3350b0b68a199f0895f549320b
Rohit Malviya <rohitmalviya2324@gmail.com>
2023-10-18 16:06:25 +0530

    JavaScript Project

--- Project-JS/script.js
+++ Project-JS/script.js
@@ -196,7 +196,7 @@ function getData() {
       id: 5,
       title: "Huawei P30",
       description:
-        "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+        "Huawe is re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
       price: 499,
       discountPercentage: 10.58,
       rating: 4.09,
